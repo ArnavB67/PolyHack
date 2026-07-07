9906b4a2324c205869daac9b4724d0978f6cf890
Arπab <85987387+ArnavB67@users.noreply.github.com>
2026-07-07 20:11:03 +0530

    Made Game UI
    Made a level Background ui with moving text and player rotation in the direction it is moving

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+const RotationSpeed = 15
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("Left","Right","Up","Down")
-	velocity = direction*SPEED
+	var Direction = Input.get_vector("Left","Right","Up","Down")
+	velocity = Direction*SPEED
+	if Direction.length()>0:
+		rotation = lerp_angle(rotation,Direction.angle(),RotationSpeed*delta)
 	move_and_slide()
